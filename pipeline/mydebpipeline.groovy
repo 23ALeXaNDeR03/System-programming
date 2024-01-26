5a517dab481c53936a220d7250a252304d365e6d
23ALeXaNDeR03 <144160144+23ALeXaNDeR03@users.noreply.github.com>
2024-01-26 17:03:51 +0200

    Update mydebpipeline.groovy

--- pipeline/mydebpipeline.groovy
+++ pipeline/mydebpipeline.groovy
@@ -6,17 +6,16 @@ pipeline {
             steps {
                 script {
                     if(!fileExists('testdebian')){
-                    sh 'mkdir testdebian'
-                    sh 'mkdir testdebian/DEBIAN'
-                    sh 'mkdir testdebian/usr'
-                    sh 'mkdir testdebian/usr/local'
-                    sh 'mkdir testdebian/usr/local/bin'
-                    sh 'wget https://github.com/23ALeXaNDeR03/system_programming/archive/main.zip'
-                    sh 'unzip main.zip'
-                    sh 'mv system_programming-main/debian/control testdebian/DEBIAN'
-                    sh 'mv system_programming-main/count_files.sh testdebian/usr/local/bin/'
-                    sh 'dpkg-deb --build testdebian'
-                }
+                        sh 'mkdir testdebian'sh 'mkdir testdebian/DEBIAN'
+                        sh 'mkdir testdebian/usr'
+                        sh 'mkdir testdebian/usr/local'
+                        sh 'mkdir testdebian/usr/local/bin'
+                        sh 'wget https://github.com/23ALeXaNDeR03/system_programming/archive/main.zip'
+                        sh 'unzip main.zip'
+                        sh 'mv system_programming-main/debian/control testdebian/DEBIAN'
+                        sh 'mv system_programming-main/count_files.sh testdebian/usr/local/bin/'
+                        sh 'dpkg-deb --build testdebian'
+                        }
                 }
             }
         }
